a9893228b8bad082e83762d4850b83aff8643f68
daed <107429396+daedx@users.noreply.github.com>
2022-11-17 09:49:24 -0800

    update

---
+++ choices.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void Intro(){}
+
+int main() {
+Intro();
+
+
+
+
+return 0;
+}
+
+void Intro(){
+    cout << "Welcome to Fosterer of the Wolf" << endl;
+    cout << ""
+}
+
