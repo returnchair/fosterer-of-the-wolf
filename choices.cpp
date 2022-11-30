27e6fb920add408ab0afc1e18f095507c74a0642
daed <107429396+daedx@users.noreply.github.com>
2022-11-30 08:36:57 -0800

    Update choices.cpp

--- choices.cpp
+++ choices.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 void Intro(){}
 void Choice(){}
-void Thorending(){}
-void Tyrending(){}
+
+void ThorEnding(){}
+void TyrEnding(){}
 void End(){}
 
 bool charcter;
@@ -57,13 +58,13 @@ void Choice(){ // character choice
     } 
 }
 
-void Thorending(){
+void ThorEnding(){
     cout << "The God of Thunder faught a hard battle against Jormungandr. Though the God victorious, the poison from the serpent slowly succumbs him. " << endl;
     cout << "The God takes nine steps foward then falls dead. Through the outcome of the death of Thor many tragic events come to past." << endl;
     cout << "Once the God of Thunder dies, Loki an the God Heimdall kill eachother, Freyr is killed by Surtr, and the gods Mani and Solveig are slain by Skoll and Hati." << endl;
 }
 
-void Tyrending(){
+void TyrEnding(){
     cout << "The God of War faught a tough battle against Garmr. The God of War manages to kill the guard dog of hel. With the death of the gods" << endl;
     cout << "Loki, Hemidall, Thor, Freyr, "
 }
