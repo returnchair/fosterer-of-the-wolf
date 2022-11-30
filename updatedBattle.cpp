5069c704904d5d8b1dbb4d09b3575828dda7df0c
daed <107429396+daedx@users.noreply.github.com>
2022-11-30 09:51:24 -0800

    Update updatedBattle.cpp

--- updatedBattle.cpp
+++ updatedBattle.cpp
@@ -1,5 +1,5 @@
 // BUG DISCOVERED
-// @ Line 34, string needs to be properly initialized | @ Line 64-91 string array needs to be checked via a array-for-loop to check if input is valid
+// @ Line 34, string needs to be properly initialized | @ Line 64-91 string array needs to be checked via a array-for-loop to check if input is valid | @ Line 49-56 Program crashes once critical occurs
 // BUG DISCOVERED, -bobby (ps, i'll work on incoporating ascii art and sound after all bugs are fixed)
 
 #include <string>
