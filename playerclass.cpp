d4621ed4e3b312256abb8088f5430e3f7f462ad4
daed <107429396+daedx@users.noreply.github.com>
2022-12-06 12:57:10 -0800

    a

--- playerclass.cpp
+++ playerclass.cpp
@@ -8,96 +8,44 @@
 
 using namespace std;
 
+#include "classes.hpp"
+
 // att is attack
 // magicAtt is magical attacks
 // health ais health
 // speed determines the probability of how much they heal and when they heal (HPRegeneration function)
+// defense substracts attack
 
-class Thor{
-    public:
+Thor::Thor(bool alive1, double attack1, double magicAttack1, double health1, double defense1, double speed1){
+    alive = alive1;
+    attack = attack1;
+    magicAttack = magicAttack1;
+    health = health1;
+    defense = defense1;
+    speed = speed1;
+}
 
-    bool player;
-    double att, magicAtt, health, speed, maxH, maxS;
-    bool alive;
-    string name = "Thor";
-   
-    Thor(){
-    att = 250; magicAtt = 450; health = 2500; defense = 110; 
-    }
- 
-    cout << "\n You have Chosen the Almighty\n        THOR         \nGod Of Thunder\n Wielder of Mjnolnir\nSon Of Odin\nFosterer of the Serpent" <<endl;
-    cout << "These are Thor's stats: " << "Attack = 250 Magic Attack = 450 Health = 2500 Speed = 10" << endl;
-    
-    bool updatehealth(double heal, double att){  // will be called in main function in a while loop
-        if(att >= heal){
-            return false;
-            cout << "Would you like to try again?" << endl;
-        } else {
-            return true;
-        }
-    }
-};
+Tyr::Tyr(bool alive1, double attack1, double heavyAttack1, double health1, double defense1, double speed1){
+    alive = alive1;
+    attack = attack1;
+    heavyAttack = heavyAttack1;
+    health = health1;
+    defense = defense1;
+    speed = speed1;
+}
 
-class Tyr{
-    public:
-     double attack, heavyAtt, health, speed;
-    bool alive;
-    bool player;
+Garm::Garm(bool alive1, double attack1, double heavyAttack1, double health1, double speed1){
+    alive = alive1;
+    attack = attack1;
+    heavyAttack = heavyAttack1;
+    health = health1;
+    speed = speed1;
+}
 
-    Tyr(){
-        attack = 250; heavyAtt = 350; health = 2000; defense = 140;
-    }
-
-    cout <<"\nYou have chosen the Relentless\n       TYR       \nGod Of War\nBinder of Fenrir\nFosterer of the Wolf" << endl;
-    cout << "These are Tyr's stats: " << "Attack = 250 Magic Attack = 350 Health = 200 Speed = 20" << endl;
-    
-    bool updatehealth(double heal, double att){  // will be called in main function in a while loop
-        if(att >= heal){
-            return false;
-            cout << "Would you like to try again?" << endl;
-        } else {
-            return true;
-        }
-    }
-};
-
-// bosses
-
-class Garm{
-    public:
-    double att, heavyAtt, health, speed;
-    bool alive;
-    string name = "Garm";
-
-    Garm(){
-        att =  250; heavyatt = 400; health = 4000; speed = 10;
-    }
-      bool updatehealth(double heal, double att){  // will be called in main function in a while loop
-        if(att >= heal){
-            return false;
-        } else {
-            return true;
-        }
-    }
-};
-
-class Jormungandr{
-    public:
-    double attack, heavyAtt, health, speed;
-    string name = "Jormungandr";
-
-    Jormungandr(){
-         att =  200; heavyatt = 450; health = 5000; speed = 5;
-    }
-    
-      bool updatehealth(double heal, double att){  // will be called in main function in a while loop
-        if(att >= heal){
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-};
-
-
+Jormungandr::Jormungandr(bool alive1, double attack1, double heavyAttack1, double health1, double speed1){
+    alive = alive1;
+    attack = attack1;
+    heavyAttack = heavyAttack1;
+    health = health1;
+    speed = speed1;
+}
