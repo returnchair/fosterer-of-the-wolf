28a40f3adaf26f5f205125490f5423469cf3a372
chair <107429396+returnchair@users.noreply.github.com>
2022-12-11 05:24:38 +0000

    oops lol

--- ascii.cpp
+++ ascii.cpp
@@ -10,7 +10,7 @@ void enemyBar(double enemyHealth, double inflictedDmg){
     double percentage = round(inflictedDmg/enemyHealth * 100);
     string bar = "****************************************************************************************************"; 
 
-    if(percentage >= 100){
+    if(percentage <= 100){
 
     for(double i = 0; i < percentage; i++){
         bar.replace(i, 1, " "); // well aware that i can do 100-i to make it look nicer, but it came with some issues that i was too lazy to fix
