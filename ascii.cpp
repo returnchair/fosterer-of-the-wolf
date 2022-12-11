d66a2dcde515fbc10067e614a2592fdcf625a925
chair <107429396+returnchair@users.noreply.github.com>
2022-12-11 05:21:57 +0000

    ascii patch

--- ascii.cpp
+++ ascii.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <string>
 #include <cmath>
+
+#include "ascii.hpp"
 
 using namespace std;
 
-double globalPercentage = 100;
-
 void enemyBar(double enemyHealth, double inflictedDmg){
-    double percentage = round((inflictedDmg/enemyHealth * 100));
-    string bar = "**************************************************"; // 50 *
+    double percentage = round(inflictedDmg/enemyHealth * 100);
+    string bar = "****************************************************************************************************"; 
+
+    if(percentage >= 100){
 
     for(double i = 0; i < percentage; i++){
-        bar.replace(100-i, 1, " ");
+        bar.replace(i, 1, " "); // well aware that i can do 100-i to make it look nicer, but it came with some issues that i was too lazy to fix
     }
     
     cout << "Enemy health: " << 100-percentage << "% [ " << bar << " ]" << endl;
+
+    } else {
+        // nothing for now (finalized)
+    }
 }
                                                                           
 
